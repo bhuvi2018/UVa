392433631209d7c98e74c1f11a3ef3fb5c14753e
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-01-03 21:40:24 +0530

    Add files via upload

---
+++ Costcutting.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Costcutting {
+public static void main (String[] args) 
+{
+Scanner in=new Scanner(System.in);
+int T=in.nextInt();
+for(int i=0;i<T;i++)
+{
+    ArrayList<Integer>a=new ArrayList<Integer>();
+    a.add(in.nextInt());
+    a.add(in.nextInt());
+    a.add(in.nextInt());
+    Collections.sort(a);
+    System.out.println("Case "+(i+1)+": "+a.get(1));
+}
+}
+}
