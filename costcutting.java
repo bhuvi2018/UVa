889c9ce18d827720535386819df663ba5f27a1f5
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-01-07 12:00:27 +0530

    Cost cutting

--- costcutting.java
+++ costcutting.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class Costcutting {
+class costcutting {
 public static void main (String[] args) 
 {
 Scanner in=new Scanner(System.in);
