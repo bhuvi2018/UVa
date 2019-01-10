0e5d9caf8a77953ed9778b4d15d52dc252b11e44
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-01-10 11:40:20 +0700

    Update y3k.java

--- y3k.java
+++ y3k.java
@@ -1,30 +1,30 @@
+/*package whatever //do not write package name here */
+
+
 import java.util.Calendar;
 import java.util.GregorianCalendar;
-import java.util.Scanner;
+import java.util.*;
 
-public class Main {
-
-	public static void main(String[] args) {
-		int n, d, m, y;
-		Scanner sc = new Scanner(System.in);
-
-		while (true) {
-			n = sc.nextInt();
-			d = sc.nextInt();
-			m = sc.nextInt();
-			y = sc.nextInt();
-
-			if (n == 0 && d == 0 && m == 0 && y == 0)
-				break;
-
-			GregorianCalendar cal = new GregorianCalendar(y, m-1, d);
-
-			cal.add(Calendar.DATE, n);
-
-			System.out.println(cal.get(Calendar.DATE) + " "
-					+ (cal.get(Calendar.MONTH)+1) + " "
-					+ cal.get(Calendar.YEAR));
+class GFG {
+	public static void main (String[] args) {
+		Scanner in=new Scanner(System.in);
+		while(true)
+		{
+		    int n=in.nextInt();
+		    int d=in.nextInt();
+		    int m=in.nextInt();
+		    int year=in.nextInt();
+		    
+		    if(n==0&&d==0&&m==0&&year==0)
+		    break;
+		    
+		    GregorianCalendar c= new GregorianCalendar(year,m-1,d);
+		    
+		   c.add(Calendar.DATE, n);
+		    
+		    System.out.println(c.get(Calendar.DATE)+" "+
+				       (c.get(Calendar.MONTH)+1)+" "+
+				       c.get(Calendar.YEAR));
 		}
 	}
-
 }
