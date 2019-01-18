5f58becdcaf3bd0e2b1cdbadbfc70046ff6ab875
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-01-18 15:01:08 +0700

    Update generating_fast.java

--- generating_fast.java
+++ generating_fast.java
@@ -1,27 +1,26 @@
+
 import java.util.*;
 
 
 class Main {
 
-public static void main(String[] args) {
-Scanner sc = new Scanner(System.in);
-int t= sc.nextInt();
-StringBuffer bf = new StringBuffer("");
-
-for (int i =0; i<t ; i++) {
- if (i>0) bf.append("\n");
- char [] a =sc.next().toCharArray();
- Arrays.sort(a);
- //String s =String.valueOf(a);
- Arrays.asList(a);
- do{
-  bf.append(new String(a)+"\n");
-  //System.out.println(new String(a));
- }while(next_permutation2(a));
+public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    int t= sc.nextInt();
+    StringBuffer bf = new StringBuffer("");
+    for (int i =0; i<t ; i++)
+    {
+        if (i>0) bf.append("\n");
+        char [] a =sc.next().toCharArray();
+        Arrays.sort(a);
+        do
+        {
+            bf.append(new String(a)+"\n");
+        }while(next_permutation2(a));
+    }
+    System.out.println(bf);
 }
-System.out.println(bf); 
-  
- }
  public static boolean next_permutation2(char a[])
     {
        for(int i = a.length-2, j; i >= 0; i--)
@@ -29,17 +28,16 @@ System.out.println(bf);
         if (a[i+1] > a[i])
                {
                 for(j = a.length-1; a[j] <= a[i]; j--);
-   swap(a, i, j);
-      for(j = 1; j <= (a.length-i)/2; j++)
-       swap(a, i+j, a.length-j);
-                      return true;
-             }
-        }
-         return false;
+                swap(a, i, j);
+                for(j = 1; j <= (a.length-i)/2; j++)
+                swap(a, i+j, a.length-j);
+                return true;
+               }
+       }
+       return false;
    }
-
-
- public static void swap(char a[], int i, int j)
+   
+public static void swap(char a[], int i, int j)
  {
   char temp = a[i];
   a[i] = a[j];
