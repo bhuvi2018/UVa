bb1478c6c7ad08ff7b37b97f06aedee5fa4a0da8
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-01-21 10:21:18 +0530

    11650

--- mirror_clock.java
+++ mirror_clock.java
@@ -1,26 +1,29 @@
+/*package whatever //do not write package name here */
+
 import java.io.*;
 import java.util.*;
 
-class Main 
-{
-	public static void main (String[] args) throws IOException
-	{
-	    BufferedReader in=new BufferedReader(new InputStreamReader(System.in));
-	    int t=Integer.parseInt(in.readLine());
-	    while(t-->0)
-	    {
-	        String s=in.readLine();
-	        int h=Integer.parseInt(s.substring(0,2));
-	        int m=Integer.parseInt(s.substring(3));
-	        
-	        m=(60-m)%60;
-	        h=12-h;
-	        if(m!=0)
-	            --h;
-            h=(h+12)%12;
-            if(h==0)
-                h=12;
-            System.out.printf("%02d:%02d\n", h, m);
-	    }
+class Main{
+	public static void main (String[] args) {
+	    		Scanner in=new Scanner(System.in);
+	    		int t=in.nextInt();
+	    		while(t-->0)
+	    		{
+	    		    String s=in.next();
+	    		    int h=Integer.parseInt(s.substring(0,2));
+	    		    int m=Integer.parseInt(s.substring(3));
+	    		    
+	    		    m=(60-m)%60;
+	    		    h=12-h;
+				
+	    		    if(m!=0)
+	    		    --h;
+				
+	    		    h=(h+12)%12;
+				
+	    		    if(h==0)
+	    		    h=12;
+	    		    System.out.printf("%02d:%02d\n", h, m);
+	    		}
 	}
 }
