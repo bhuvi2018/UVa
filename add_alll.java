7a2e630036f13e5da8f9550ed0bde4c685a535c1
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-01-25 16:20:27 +0530

    10954

---
+++ add_alll.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	
+	public static void main (String [] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		while (!(s=br.readLine()).equals("0")) {
+			
+			PriorityQueue<Integer> num=new PriorityQueue<>();
+			int N=Integer.parseInt(s);
+			StringTokenizer st=new StringTokenizer(br.readLine());
+			for (int i=0;i<N;i++) 
+			num.offer(Integer.parseInt(st.nextToken()));
+			
+			int totalCost=0;
+			while (!num.isEmpty()) {
+				int cost=num.poll();
+				if (!num.isEmpty()) {
+					cost+=num.poll();
+					totalCost+=cost;
+					num.offer(cost);
+				}
+			}
+			
+			System.out.println(totalCost);
+		}
+		
+	}
+}
