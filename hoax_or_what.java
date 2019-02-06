48b4c64b6a47f8241836e0203444ad7b91b0a774
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-02-06 14:39:24 +0530

    Create hoax_or_what.java

---
+++ hoax_or_what.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	
+	public static void main (String [] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s;
+		
+		while (!(s=br.readLine()).equals("0")) {
+			int N=Integer.parseInt(s);
+			long cost=0; //wtf.
+			TreeMap<Integer,Integer> map=new TreeMap<>();
+			for (int i=0;i<N;i++) {
+				StringTokenizer st=new StringTokenizer(br.readLine());
+				int k=Integer.parseInt(st.nextToken());
+				for (int i2=0;i2<k;i2++) {
+					int num=Integer.parseInt(st.nextToken());
+					map.put(num,map.getOrDefault(num,0)+1);
+				}
+				
+				int highest=map.lastKey();
+				if (map.get(highest)==1) map.remove(highest);
+				else map.put(highest,map.get(highest)-1);
+				
+				int lowest=map.firstKey();
+				if (map.get(lowest)==1) map.remove(lowest);
+				else map.put(lowest,map.get(lowest)-1);
+				
+				cost+=(highest-lowest);
+			}
+			System.out.println(cost);
+		}
+	}
+}
