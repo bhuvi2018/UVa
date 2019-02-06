388fafc10d3b3426abb032ae401a6921b0b24003
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-02-06 15:39:57 +0530

    Update jolly_jumpers.java

--- jolly_jumpers.java
+++ jolly_jumpers.java
@@ -1,4 +1,37 @@
+import java.util.*;
 import java.io.*;
+import java.lang.*;
+class Main 
+{
+    public static void main(String [] args)
+    {
+        Scanner in=new Scanner(System.in);
+        while(in.hasNext())
+        {
+            int n=in.nextInt();
+            int a[]=new int[n];
+            for(int i=0;i<n;i++)
+                    a[i]=in.nextInt();    
+            HashSet<Integer> hs=new HashSet<Integer>();
+            for(int i=0;i<n-1;i++)
+            {
+                int abs=Math.abs(a[i+1]-a[i]);
+                if(abs<=a.length-1 && abs>0)
+                    hs.add(abs);
+                else
+                    {break;}
+                    
+            }
+            if(hs.size()==a.length-1)
+                System.out.println("Jolly");
+            else    
+                System.out.println("Not jolly");
+            
+        }
+        
+    }
+}
+/*import java.io.*;
 import java.util.*;
 
 class Main 
@@ -36,3 +69,4 @@ class Main
 	    }
 	}
 }
+*/
