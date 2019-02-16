fca4c526e40b587466011174345b330649aba19b
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-02-16 11:26:41 +0530

    Create Longest_consecutive_1's.java

---
+++ Longest_consecutive_1's.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Main
+ {
+	public static void main (String[] args)
+	 {
+	 Scanner in=new Scanner(System.in);
+		int t=in.nextInt();
+		while(t-->0)
+		{
+		    int n=in.nextInt();
+		    String s=Integer.toBinaryString(n);
+		    int c=0,max=0;
+		    for(int i=0;i<s.length();i++)
+		    {
+		        if(s.charAt(i)=='1')
+		        c++;
+		        else
+		        c=0;
+		        if(c>0&&max<c)
+		        max=c;
+		    }
+		    System.out.println(max);
+		}
+	 }
+}
